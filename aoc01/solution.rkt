453d21fe35ef753dafaec906a7a4e4a5914b65c2
Finn Günther <mail@kawzeg.com>
2020-12-02 17:32:56 +0100

    Day 1: Part 2
    In Racket

--- aoc01/solution.rkt
+++ aoc01/solution.rkt
@@ -2,4 +2,4 @@
 
 (define input (file->list "input"))
 
-(println (apply * (sequence-ref (sequence-filter (lambda (x) (= (apply + x) 2020)) (in-combinations input 2)) 0)))
+(println (apply * (sequence-ref (sequence-filter (lambda (x) (= (apply + x) 2020)) (in-combinations input 3)) 0)))
