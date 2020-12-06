e31fe932bc255ebb961a0b89502a08c554323d73
Finn Günther <mail@kawzeg.com>
2020-12-06 13:57:37 +0100

    Day 6: Part 2
    In AWK

--- aoc06/solve.sh
+++ aoc06/solve.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-awk 'BEGIN{RS="\n\n"}{
+
+echo Part 1
+awk 'BEGIN {RS="\n\n"}
+{
 gsub(/\n/,"")
 gsub(/./,"& ")
 n = split($0, a, FS)
@@ -12,3 +15,24 @@ for (i=1; i<=n; i++)
 sum += j
 }
 END {print sum}' input
+
+echo Part 2
+awk 'BEGIN {RS="\n\n"; sum=0}
+{
+gsub(/\n/," ")
+n = split($0, a, FS)
+$0=""
+k=1
+delete seen
+for (i=1; i<=n; i++) {
+    gsub(/./,"& ",a[i])
+    m = split(a[i], b, FS)
+    for (j=1; j<=m; j++)
+        seen[b[j]]+=1
+}
+for (q in seen)
+    if (seen[q] == n)
+       sum++
+}
+END {print sum}
+' input
