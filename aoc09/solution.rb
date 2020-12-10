108ea660257bbf86aff860307d6c2b0a30eea318
Finn Günther <mail@kawzeg.com>
2020-12-10 19:32:14 +0100

    Day 9: Add slightly golfed solution

--- aoc09/solution.rb
+++ aoc09/solution.rb
@@ -2,12 +2,17 @@ xs = File.open("input").read.split("\n").map(&:to_i)
 
 preamble = 25
 
-target_index = (preamble..xs.size).each{ |i|
-  break i if !xs[i-preamble..i].combination(2).map(&:sum).include? xs[i]
+target_index = (preamble..).detect{ |i|
+  !xs[i-preamble..i].combination(2).map(&:sum).include? xs[i]
 }
 p xs[target_index]
-p target_index.times.each{ |i|
-  sums = target_index.times.map{|j| xs[j,i]}.map(&:sum)
-  j = sums.find_index xs[target_index]
-  break xs[j,i].min + xs[j,i].max if j
+p (2..target_index).each{ |s|
+  j = (0..target_index).detect{|k|xs[k,s].sum==xs[target_index]}
+  break xs[j,s].min + xs[j,s].max if j
 }
+
+# Slightly golfed version
+xs = File.open("input").read.split("\n").map(&:to_i)
+p xs[i=(25..).detect{|i|!xs[i-25,25].combination(2).map(&:sum).include? xs[i]}]
+p (2..i).each{|j|k=(0..i).detect{|k|xs[k,j].sum==xs[i]}
+  break xs[k,j].min+xs[k,j].max if k}
